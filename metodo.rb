0563e9bcef332f0f4a2d899c1ba38ee58279eb95
Gabriel <102559742+gabchav@users.noreply.github.com>
2022-04-14 13:58:17 -0400

    Update metodo.rb

--- metodo.rb
+++ metodo.rb
@@ -1,13 +1,19 @@
 def acronimo(texto)
-    
-texto=texto.strip.split(/\s+/)
-a=''
-texto.each do |palabra|
-    a<<palabra[0..0]
-    
-end
-return a
+  texto = texto.strip.split(/\s+/)
+  a = ''
+  texto.each do |palabra|
+    a << palabra[0..0]
+  end
+  a.upcase
 end
 
+puts 'Ejemplo: ', acronimo('Chief Executive Officer')
 
-puts acronimo("Chief Executive Officer")
+loop do
+  print 'Ingrese una Frase (escriba "x" para terminar):'
+  f = gets.chomp
+  break if f.upcase == 'X'
+
+  puts acronimo(f)
+end
+p 'Saliendo...'
